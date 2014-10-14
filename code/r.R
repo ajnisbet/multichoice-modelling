1e3d1ac256a8f7cd3e0c88cf350ddecc659577a3
Andrew Nisbet <ajnisbet@gmail.com>
2014-10-14 13:31:44 +1300

    Initial commit

---
+++ code/r.R
@@ -0,0 +1,37 @@
+# Clear console
+rm(list = ls())
+cat("\014")
+set.seed(1)
+setwd("/mnt/data/Dropbox/Uni/2014/STAT 462/Multichoice Project")
+
+
+# Load data
+df <- read.csv('df.csv', header=TRUE)
+df$is_correct <- as.logical(df$is_correct)
+df$is_first <- as.logical(df$is_first)
+df$is_float <- as.logical(df$is_float)
+df$is_fraction <- as.logical(df$is_fraction)
+df$is_integer <- as.logical(df$is_integer)
+df$is_last <- as.logical(df$is_last)
+df$is_none_of_the_above <- as.logical(df$is_none_of_the_above)
+df$is_numeric <- as.logical(df$is_numeric)
+df$is_percent <- as.logical(df$is_percent)
+df$is_unit <- as.logical(df$is_unit)
+print(summary(df))
+
+# Split into testing and training
+training.size <- round(0.75 * nrow(df))
+training.indices <- sample(nrow(df), training.size)
+training.data <- df[training.indices, ]
+testing.data <- df[-training.indices, ]
+
+
+# Fit model
+lm <- glm(is_correct ~ is_first, data=training.data, family="binomial")
+
+# Obtain prediction
+predictions <- rep(NA, nrow(testing.data))
+probs <- predict(lm, testing.data, type="response")
+predictions[probs > 0.5] <- "Yes"
+predictions[probs < 0.5] <- "No"
+print(mean(predictions != testing.data$default))
